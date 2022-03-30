25b093d77c5cbd8ef486b28741937742c16ce12f
Hank liang <henki81227@gmail.com>
2022-03-30 14:22:23 +0800

    add babelrc

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Vue tutorial
 ## 2022/03/30
 ## node.js 16
+
+## babel 用來將開發好的code轉譯到適合所有瀏覽器
---
+++ src/lab1_var_let_const.js
@@ -0,0 +1,8 @@
+let x = 5
+console.log(x)
+x += 1
+console.log(x)
+const y = 'hello world x=5'
+console.log(y)
+const z = `hello world x=${x}`
+console.log(z)
